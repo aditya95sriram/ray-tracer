453287f3d6b8882ef78829beb159e5fa4dc484b2
Aditya Sriram <aditya95sriram@gmail.com>
2019-02-26 12:54:22 +0530

    Add drag obstacles, shadow mode, help screen
    Click and drag obstacles, added shadow modes - histogram and alpha bars, 
    added help screen (press h)

--- sketch.js
+++ sketch.js
@@ -7,23 +7,37 @@ let offset_x = 50;
 let mouse_start;
 let source_color = 'yellow',
     obstacle_color = 'lightblue',
+    obstacle_hover_color = 'blue',
     screen_color = 'pink';
-let resolution = 0.5;  // percentage of pixels rendered (max 1)
+let resolution = 1;  // percentage of pixels rendered (max 1)
 let show_guidelines = true;
+let hovering=false, hover_start;
 let fps_prev = new Date(), fps_cur, fps;
+let shadow_mode = 1;
 let frame_ctr = 0;
 
 function Obstacle(start, end) {
   this.x1 = this.x2 = start.x;  // same x coord
   this.y1 = min(start.y, end.y);
   this.y2 = max(start.y, end.y);
+  this.length = dist(this.x1, this.y1, this.x2, this.y2);
+  this.hover = false;
 
   this.draw = function() {
     push();
-    stroke(obstacle_color);
+    stroke(this.hover ? obstacle_hover_color : obstacle_color);
     strokeWeight(2);
     line(this.x1, this.y1, this.x2, this.y2);
     pop()
+  }
+
+  this.check = function(pt) {
+    pt2 = pt.copy();
+    // lies within ellipse
+    let d1 = pt.sub(this.x1, this.y1).mag();
+    let d2 = pt2.sub(this.x2, this.y2).mag();
+    //console.log(d1, d2, this.length);
+    return d1 + d2 <= 1.1*this.length;
   }
 }
 
@@ -35,6 +49,7 @@ function init() {
   source_top = createVector(source_pos.x, source_pos.y-source_height/2);
   source_bot = createVector(source_pos.x, source_pos.y+source_height/2);
   show_guidelines = true;
+  document.body.style.cursor = "pointer";
 }
 
 function setup() {
@@ -72,10 +87,10 @@ function get_intensities() {
     for (let ob of obstacles) {
       let start_y = (ob.y1 - y)*(screen_x - ob.x1)/(ob.x1 - 0) + ob.y1;
       let end_y = (ob.y2 - y)*(screen_x - ob.x2)/(ob.x2 - 0) + ob.y2;
-      //console.log(start_y, end_y);
-      start_y += max(0, screen_height/2);
-      end_y += min(screen_height, screen_height/2);
-      //console.log(start_y, end_y);
+      // if (y==0) console.log(start_y, end_y);
+      start_y = max(0, start_y + screen_height/2);
+      end_y = min(screen_height, end_y + screen_height/2);
+      // if (y==0) console.log(start_y, end_y);
       for (let i=start_y*resolution; i<=end_y*resolution; i++) {
         occ[parseInt(i)] = 0;
       }
@@ -87,17 +102,20 @@ function get_intensities() {
   push();
   translate(screen_x, -screen_height/2);
   noStroke();
-  /*fill(source_color);
-  for (v of scr) {
-    rect(0, 0, v, 1/resolution);
-    translate(0, 1/resolution);
-  }*/
-  let c = color(source_color);
-  for (v of scr) {
-    c.setAlpha(255*v/source_height);
-    fill(c);
-    rect(0, 0, 50, 1/resolution);
-    translate(0, 1/resolution);
+  if (shadow_mode == 0) {
+    fill(source_color);
+    for (v of scr) {
+      rect(0, 0, v, 1/resolution);
+      translate(0, 1/resolution);
+    }
+  } else {
+    let c = color(source_color);
+    for (v of scr) {
+      c.setAlpha(255*v/source_height);
+      fill(c);
+      rect(0, 0, 50, 1/resolution);
+      translate(0, 1/resolution);
+    }
   }
   pop();
 }
@@ -108,6 +126,25 @@ function draw_guidelines() {
   line(source_top.x, source_top.y, n.x, n.y);
   n = p5.Vector.sub(p, source_bot).setMag(1200).add(source_bot);
   line(source_bot.x, source_bot.y, n.x, n.y);
+}
+
+function check_obstacles() {
+  document.body.style.cursor = 'pointer';
+  hovering = false;
+  obstacles.map(x => x.hover = false);
+  for (let ob of obstacles) {
+    if (ob.check(getMouse())) {
+      document.body.style.cursor = 'grab';
+      ob.hover = true;
+      hover_start = getMouse();
+      hovering = ob;
+      return;
+    }
+  }
+}
+
+function mouseMoved() {
+  check_obstacles();
 }
 
 function draw() {
@@ -127,6 +164,7 @@ function draw() {
   draw_source();
   draw_screen();
   draw_obstacles();
+  check_obstacles();
   get_intensities();
   if (show_guidelines) draw_guidelines();
   noStroke();
@@ -138,22 +176,45 @@ function draw() {
 }
 
 function mousePressed() {
-  mouse_start = getMouse();
+  if (!hovering)
+    mouse_start = getMouse();
 }
 
 function mouseReleased() {
-  obstacles.push(new Obstacle(mouse_start, getMouse()));
-  mouse_start = undefined;
+  if (typeof(mouse_start) != 'undefined') {
+    obstacles.push(new Obstacle(mouse_start, getMouse()));
+    mouse_start = undefined;
+  }
+}
+
+function mouseDragged(event) {
+  if (hovering) {
+    delta = p5.Vector.sub(getMouse(), hover_start);
+    hovering.x1 += delta.x;
+    hovering.x2 += delta.x;
+    hovering.y1 += delta.y;
+    hovering.y2 += delta.y;
+  } else {
+    document.body.style.cursor = "pointer";
+  }
 }
 
 function keyPressed() {
   if (key == 'z' && obstacles.length) trash.push(obstacles.pop());
   if ((key == 'Z' || key == 'x') && trash.length) obstacles.push(trash.pop());
   if (key == 'g') show_guidelines = !show_guidelines;
+  if (key == 's') shadow_mode = 1-shadow_mode;
+  if (key == 'h') alert(" z - undo \n shift+z|x - redo \n g - guidelines \n \
+s - shadow mode \n h - help screen");
 }
 
 
 /*
 Notes:
 {x2: 854, x1: 854, y1: -238, y2: -149}
+// perpendicular distance of point from line
+let d = Math.abs((this.y2 - this.y1)*pt.x - (this.x2 - this.x1)*pt.y +
+this.x2*this.y1 - this.x1*this.y2) /
+(Math.sqrt((this.y2-this.y1)**2 + (this.x2-this.x1)**2));
+// return d;
 */
